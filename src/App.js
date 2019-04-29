abbdad50b69da15c84d89e46ecd10c4bef2fe1c7
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-04-29 14:54:27 +0200

    init React workshop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NamesList from './NamesList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <NamesList listType = {"ul"} listOfNames = {["C#", "Java", "C++"]} />
+        <NamesList listType = {"ol"} listOfNames = {["Python", "Basic", "Pascal"]} />
+      </div>
     </div>
   );
 }
---
+++ src/NamesList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NameList = (props) => {
+    return (
+        <props.listType>
+            {
+                props.listOfNames.map((row, index) =>
+                <li key= {index}>
+                    <p>{row}</p>
+                </li>
+                )
+            }
+        </props.listType>
+    );
+}
+
+
+export default NameList;
