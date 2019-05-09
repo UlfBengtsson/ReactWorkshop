b9783ed77212fb6d9c299e45576cf2c06421f975
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-05-09 12:23:19 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import NamesList from './NamesList';
+import React, { Component } from "react";
+import "./App.css";
+import NamesList from "./NamesList";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-      <div>
-        <NamesList listType = {"ul"} listOfNames = {["C#", "Java", "C++"]} />
-        <NamesList listType = {"ol"} listOfNames = {["Python", "Basic", "Pascal"]} />
+class App extends Component {
+  state = {
+    codeLang: [],
+    dataFail: false,
+    dataMsg: null
+  };
+
+  componentDidMount() {
+    axios
+      .get(`https://localhost:44345/api/values`)
+      .then(res => {
+        const codeLang = res.data;
+        this.setState({ codeLang });
+      })
+      .catch(
+        function(error) {
+          this.setState({ dataFail: true });
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            this.setState({
+              dataMsg: "Sever Access Denied: " + error.response.status
+            });
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            this.setState({ dataMsg: "Sever dident respond" });
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            this.setState({ dataMsg: "Failed to send request" });
+          }
+        }.bind(this)
+      );
+  }
+
+  render() {
+    const { codeLang, dataFail, dataMsg } = this.state;
+
+    return (
+      <div className="App">
+        <div>
+          <NamesList listType={"ul"} listOfNames={this.state.codeLang} />
+          <NamesList listType={"ol"} listOfNames={codeLang} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/NamesList.js
+++ src/NamesList.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const NameList = (props) => {
-    return (
-        <props.listType>
-            {
-                props.listOfNames.map((row, index) =>
-                <li key= {index}>
-                    <p>{row}</p>
-                </li>
-                )
-            }
-        </props.listType>
-    );
-}
-
+const NameList = props => {
+  return (
+    <props.listType>
+      {props.listOfNames.map((row, index) => (
+        <li key={index}>
+          <p>{row}</p>
+        </li>
+      ))}
+    </props.listType>
+  );
+};
 
 export default NameList;
